8474ed849f7787561b10ce16d3f8f323296da01f
elmsec <dev@elma.dev>
2022-05-28 15:51:47 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:timer/timer/view/timer_page.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Timer',
+      theme: ThemeData(
+        primaryColor: Color.fromRGBO(109, 234, 255, 1),
+        colorScheme: ColorScheme.light(
+          secondary: Color.fromRGBO(72, 74, 126, 1),
+        ),
+      ),
+      home: const TimerPage(),
+    );
+  }
+}
